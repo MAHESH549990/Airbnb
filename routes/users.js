c65e4770d6f30d1978688443e0ee775b9587f212
Mahesh Bhatt <mohanbhatt805777@gmail.com>
2026-09-12 11:55:41 +0530

    Updated some structure of files

--- routes/users.js
+++ routes/users.js
@@ -24,7 +24,7 @@ router.post("/signup",asyncWrap(async(req,res)=>{
       })
   }
   catch(err){
-      req.flash("failure",err.message);
+      req.flash("error",err.message);
       res.redirect("/user/signup");
   }
 }));
